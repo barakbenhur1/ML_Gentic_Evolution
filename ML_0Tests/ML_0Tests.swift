ef203fd190b3abc0def6e43d7d73d865d8d9a43f
barak ben hur <barakbenhur@gmail.com>
2021-06-21 12:23:55 +0300

    Initial Commit

---
+++ ML_0Tests/ML_0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ML_0Tests.swift
+//  ML_0Tests
+//
+//  Created by Interactech on 21/06/2021.
+//
+
+import XCTest
+@testable import ML_0
+
+class ML_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
